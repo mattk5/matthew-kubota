14d3a57aefad9d90dbd34b043d0c7e3333cb8f06
Matt <mattkubota5@gmail.com>
2017-08-31 23:13:13 -0400

    created routes for the projects page. added some styling to the index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact 
+  get 'contact' => 'pages#contact', as: :contact
+  get 'projects' => 'pages#projects', as: :projects 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
