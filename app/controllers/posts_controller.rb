d793a6b353a299e9b47d9cfa8687a51349d63d0f
Matt <mattkubota5@gmail.com>
2017-09-07 17:25:40 -0400

    updated admin and user views when dealing with blog posts. added text and formatting to the about section.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: [:show, :index]
 
   # GET /posts
   # GET /posts.json
@@ -14,6 +15,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @posts = Post.most_recent
   end
 
   # GET /posts/new
@@ -68,7 +70,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = current_admin.posts.friendly.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
