ec67ea7dc41543cdddc6f5585ae73b977fea51dc
Matt <mattkubota5@gmail.com>
2017-09-07 11:04:14 -0400

    created an index action in the pages controller and created an index page to allow a separate blog page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
-  root 'posts#index'
+  root 'pages#index'
   get 'about' => 'pages#about', as: :about
   get 'contact' => 'pages#contact', as: :contact
-  get 'projects' => 'pages#projects', as: :projects 
+  get 'projects' => 'pages#projects', as: :projects
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
