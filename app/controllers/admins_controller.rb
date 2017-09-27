d436add1135bc07589fab0b0f796684ad0ddeab8
Matt <mattkubota5@gmail.com>
2017-09-27 18:56:00 -0400

    fixed syntax error

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
-class AdminsController << ApplicationController
+class AdminsController < ApplicationController
   before_action :authenticate_author!
 end
 
-# May have to move the 'posts' view folder into the 'admin' views folder to ensure all the styling is working correctly. Might have to go back through this episode and fix naming of route paths. Also have to create an admin within the console. 
+# May have to move the 'posts' view folder into the 'admin' views folder to ensure all the styling is working correctly. Might have to go back through this episode and fix naming of route paths. Also have to create an admin within the console.
